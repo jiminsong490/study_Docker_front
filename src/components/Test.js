578cc01a99ead72c1e354c79adf9c5aa55605c01
Song <misosjm@naver.com>
2022-09-28 23:57:29 +0900

    test

--- src/components/Test.js
+++ src/components/Test.js
@@ -12,8 +12,8 @@ const Test = () => {
         <>
             <button onClick={a}>클릭</button>
             <p>{result}</p>
-            <p>github actios 성공</p>
-            <p>test</p>
+            <p>github a ctios 성공</p>
+            <p>test </p>
         </>
     )
 }
